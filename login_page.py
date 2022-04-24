64efcbd623b984f4ee3b6cdaee18d61818f1f025
Norbert Acedanski <norbert.acedanski@gmail.com>
2022-04-24 20:15:19 +0200

    Initial commit - working refreshing of offers from Sprzedajemy.pl

---
+++ login_page.py
@@ -0,0 +1,46 @@
+class LoginSprzedajemy:
+    textbox_email_xpath = "//input[@type='email']"
+    textbox_password_xpath = "//input[@type='password']"
+    button_login_xpath = "//button[@type='submit']"
+    button_logout_xpath = "//a[@class='logout']"
+    def __init__(self, driver):
+        self.driver = driver
+
+    def set_email(self, email):
+        self.driver.find_element_by_xpath(self.textbox_email_xpath).clear()
+        self.driver.find_element_by_xpath(self.textbox_email_xpath).send_keys(email)
+
+    def set_password(self, password):
+        self.driver.find_element_by_xpath(self.textbox_password_xpath).clear()
+        self.driver.find_element_by_xpath(self.textbox_password_xpath).send_keys(password)
+
+    def click_login(self):
+        self.driver.find_element_by_xpath(self.button_login_xpath).click()
+
+    def click_logout(self):
+        self.driver.find_element_by_xpath(self.button_logout_xpath).click()
+
+class MyOffersSprzedajemy:
+    button_cookies_xpath = "//button [@id='didomi-notice-agree-button']"
+    button_next_page_xpath = "//li[@class='next']"
+    number_of_pages_xpath = "//ul[@class='cntPaginator'] /li[last() - 1] //span"
+    number_of_offers_in_a_page = "//input[@name='limit']"
+    checkbox_check_all_xpath = "//a[@class='selectDeselectAll'] /span"
+    button_refresh_xpath = "//div[@class='theButton theButtonNoBg theButtonRefresh'] /a"
+    def __init__(self, driver):
+        self.driver = driver
+    
+    def agree_to_cookies(self):
+        self.driver.find_element_by_xpath(self.button_cookies_xpath).click()
+
+    def find_number_of_pages(self):
+        return int(self.driver.find_element_by_xpath(self.number_of_pages_xpath).text)
+
+    def check_all_offers(self):
+        self.driver.find_element_by_xpath(self.checkbox_check_all_xpath).click()
+
+    def refresh_offers(self):
+        self.driver.find_element_by_xpath(self.button_refresh_xpath).click()
+    
+    def go_to_next_page(self):
+        self.driver.find_element_by_xpath(self.button_next_page_xpath).click()
---
+++ refreshing_offers.py
@@ -0,0 +1,53 @@
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchWindowException, NoSuchElementException
+
+from login_page import LoginSprzedajemy, MyOffersSprzedajemy
+from getpass import getpass
+import time
+
+class RefreshingSprzedajemy():
+    sprzedajemy_url = "https://sprzedajemy.pl/Twoje-Oferty"
+
+    def get_credentials(self):
+        print("Refreshing sprzedajemy offers")
+        self.email = input("Provide your email: ")
+        self.password = getpass("Provide your password: ")
+    
+    def launch_page(self):
+        self.driver = webdriver.Chrome("./resources/chromedriver.exe")
+        self.driver.maximize_window()
+        self.driver.get(self.sprzedajemy_url)
+        page_title = self.driver.title
+        if "Sprzedajemy.pl" not in page_title or "Logowanie do serwisu" not in page_title:
+            raise NoSuchWindowException("It appears, that the site is not correct")
+    
+    def login(self):
+        login_page = LoginSprzedajemy(self.driver)
+        login_page.set_email(self.email)
+        login_page.set_password(self.password)
+        login_page.click_login()
+        page_title = self.driver.title
+        if "Sprzedajemy.pl" not in page_title or "Twoje ogłoszenia" not in page_title:
+            raise NoSuchWindowException("It appears, that the site is not correct")
+    
+    def refresh_offers(self):
+        my_offers = MyOffersSprzedajemy(self.driver)
+        try:
+            my_offers.agree_to_cookies()
+        except NoSuchElementException:
+            time.sleep(1)
+            my_offers.agree_to_cookies()
+        number_of_pages = my_offers.find_number_of_pages()
+        for page in range(1, number_of_pages + 1):
+            my_offers.check_all_offers()
+            my_offers.refresh_offers()
+            if page != number_of_pages:
+                my_offers.go_to_next_page()
+        
+
+if __name__ == "__main__":
+    refreshing_sprzedajemy_offers = RefreshingSprzedajemy()
+    refreshing_sprzedajemy_offers.get_credentials()
+    refreshing_sprzedajemy_offers.launch_page()
+    refreshing_sprzedajemy_offers.login()
+    refreshing_sprzedajemy_offers.refresh_offers()
